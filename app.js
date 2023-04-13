12677c9d24389f9a36facb74221163d45e5e00a6
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-04-13 14:36:22 +0100

    added computer selection function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const game = () => {
   let pScore = 0;
   let cScore = 0;
 
+  // start the game
   const startGame = () => {
     const playBtn = document.querySelector('.intro button');
     const introScreen = document.querySelector('.intro');
@@ -9,11 +10,29 @@ const game = () => {
 
     playBtn.addEventListener('click', () => {
       introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+  // play match
+  const playMatch = () => {
+    const options = document.querySelectorAll('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+    // computers options
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+    options.forEach(option => {
+      option.addEventListener('click', function() {
+        // computer choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+      });
     });
   };
 
   // call all inner function
   startGame();
+  playMatch();
 };
 
 // start the game function
